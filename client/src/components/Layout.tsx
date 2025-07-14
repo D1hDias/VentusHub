433b507505bd39526f0a496e52a85f2e3df14307
Diego Dias <diego@ventushub.com>
2025-07-14 03:53:41 -0300

    ddd

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -129,7 +129,7 @@ export default function Layout({ children }: LayoutProps) {
                 <Menu className="h-5 w-5" />
               </Button>
               <img 
-                src="VentusHub\client\src\assets\logo.png" 
+                src="client\src\assets\logo.png" 
                 alt="Ventus Hub" 
                 className="w-[120px] h-auto"
               />
